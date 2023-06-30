1d7229b61162e90bd1c077b5100187f0c2ed01dc
andradegeovana <geovanaandrade@edu.unifil.br>
2023-06-30 15:54:35 -0300

    tudo funcionando

--- telaCheia.js
+++ telaCheia.js
@@ -1,5 +1,29 @@
 function fullScreen() {
+    var div = document.getElementById("divframejitsi");
     var jitsi = document.getElementById("framejitsi");
+    var botaoFS = document.getElementById("fullscreenButton");
+    var botaoSS = document.getElementById("smallscreenButton");
+
+    div.style.width = "90vw";
+    div.style.height = "99vh";
     jitsi.style.width = "90vw";
     jitsi.style.height = "99vh";
+    botaoFS.style.zIndex = "50";
+    botaoSS.style.zIndex = "100";
   }
+
+function smallScreen() {
+  var div = document.getElementById("divframejitsi");
+  var jitsi = document.getElementById("framejitsi");
+  var botaoFS = document.getElementById("fullscreenButton");
+  var botaoSS = document.getElementById("smallscreenButton");
+
+  div.style.width = "25vw";
+  div.style.height = "35vh";
+  jitsi.style.width = "25vw";
+  jitsi.style.height = "35vh";
+  div.style.justifyContent = "flex-end";
+  div.style.alignItems = "flex-end";
+  botaoFS.style.zIndex = "100";
+  botaoSS.style.zIndex = "50";
+}
